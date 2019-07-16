7fdc747dbe7b104d401584471a9e2b591f01fdde
liuxuan <liuxuan@liuxuandeMacBook-Pro.local>
2019-07-16 14:48:53 +0800

    First Commit

---
+++ Example/Pods/Target Support Files/MyWeiboHomePage/MyWeiboHomePage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyWeiboHomePage : NSObject
+@end
+@implementation PodsDummy_MyWeiboHomePage
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyWeiboHomePage_Tests/Pods-MyWeiboHomePage_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyWeiboHomePage_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyWeiboHomePage_Tests
+@end
